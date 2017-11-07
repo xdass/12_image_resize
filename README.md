79c94f96fc07e5bcb4d45854cb95d71f9b191be0
xdass <420600@gmail.com>
2017-11-07 15:45:23 +0500

    Init commit

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Image Resizer
 
-[TODO. There will be project description]
+This program can resize images by params such as:
+* width
+* height
+* scale
 
+# Hot to istall
+You need to install Pillow.
+```bash
+pip install -r requirements.txt
+```
+
+# How to use
+```bash
+$ python image_resize.py --width 500 --height 300 <image_file_name>
+```
+To get more information use:
+```bash
+$ python image_resize.py -h
+```
+It's create resized image file in the image directory and name like this <image_file_name>__500x312.jpg
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,67 @@
+from PIL import Image
+import argparse
+import os
+from math import floor
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def open_image(image_path):
+    try:
+        image = Image.open(image_path)
+        return image
+    except FileNotFoundError:
+        return None
 
+
+def save_image(image, default_path=None):
+    image.save(default_path)
+
+
+def generate_path_to_image(image_path, new_image):
+    orig_filename, orig_file_ext = os.path.splitext(image_path)
+    new_filename = '{}__{}x{}{}'.format(orig_filename, new_image.width, new_image.height, orig_file_ext)
+    return new_filename
+
+
+def resize_image(image_to_resize, width, height, scale):
+    result_image = None
+    original_width, original_height = image_to_resize.size
+    if scale and (height or width):
+        raise ValueError('You can`t use --scale option with width and height arguments!')
+    elif scale:
+        result_image = image_to_resize.resize((int(original_width * scale), int(original_height * scale)))
+    elif width is None:
+        resize_width = original_width / (original_height / height)
+        result_image = image_to_resize.resize((floor(resize_width), height))
+    elif height is None:
+        resize_height = original_height / (original_width / width)
+        result_image = image_to_resize.resize((width, floor(resize_height)))
+    elif width and height:
+        result_image = image_to_resize.resize((width, height))
+    return result_image
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description='Image Resizer')
+    parser.add_argument('orig_image_path', help='path to original image')
+    parser.add_argument('--width',
+                        type=int,
+                        help='Image resize width. You cant use --scale with this option')
+    parser.add_argument('--height',
+                        type=int,
+                        help='Image resize height. You cant use --scale with this option')
+    parser.add_argument('--scale',
+                        type=float,
+                        help='Zoom ratio. If you use this option you cant use --width and --height')
+    parser.add_argument('--output',
+                        help='Resized file destination. If this option not used, the file saved near the original file')
+    args = parser.parse_args()
+
+    original_image = open_image(args.orig_image_path)
+    if original_image is not None:
+        resized_image = resize_image(original_image, args.width, args.height, args.scale)
+        if args.output is None:
+            new_image_path = generate_path_to_image(args.orig_image_path, resized_image)
+        else:
+            new_image_path = args.output
+        save_image(resized_image, new_image_path)
+    else:
+        print('File {} not found'.format(args.orig_image_path))
