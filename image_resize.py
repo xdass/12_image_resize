a1622d01edfda05a71db53139fcb882f64186b37
xdass <420600@gmail.com>
2017-11-16 10:32:11 +0500

    save_image function replaced by Pillow save method. Arguments check move to argparser init section.

--- image_resize.py
+++ image_resize.py
@@ -12,10 +12,6 @@ def open_image(image_path):
         return None
 
 
-def save_image(image, default_path=None):
-    image.save(default_path)
-
-
 def generate_path_to_image(image_path, new_image):
     orig_filename, orig_file_ext = os.path.splitext(image_path)
     new_filename = '{}__{}x{}{}'.format(orig_filename, new_image.width, new_image.height, orig_file_ext)
@@ -25,9 +21,7 @@ def generate_path_to_image(image_path, new_image):
 def resize_image(image_to_resize, width, height, scale):
     result_image = None
     original_width, original_height = image_to_resize.size
-    if scale and (height or width):
-        raise ValueError('You can`t use --scale option with width and height arguments!')
-    elif scale:
+    if scale:
         result_image = image_to_resize.resize((int(original_width * scale), int(original_height * scale)))
     elif width is None:
         resize_width = original_width / (original_height / height)
@@ -44,16 +38,25 @@ if __name__ == '__main__':
     parser.add_argument('orig_image_path', help='path to original image')
     parser.add_argument('--width',
                         type=int,
-                        help='Image resize width. You cant use --scale with this option')
+                        help='Image resize width.'
+                             'You cant use --scale with this option')
     parser.add_argument('--height',
                         type=int,
-                        help='Image resize height. You cant use --scale with this option')
+                        help='Image resize height. '
+                             'You cant use --scale with this option')
     parser.add_argument('--scale',
                         type=float,
-                        help='Zoom ratio. If you use this option you cant use --width and --height')
+                        help='Zoom ratio.'
+                             'If you use this option'
+                             ' you cant use --width and --height')
     parser.add_argument('--output',
-                        help='Resized file destination. If this option not used, the file saved near the original file')
+                        help='Resized file destination. '
+                             'If this option not used, '
+                             'the file saved near the original file')
     args = parser.parse_args()
+    if args.scale and args.height or args.width:
+        parser.error("You can`t use --scale option "
+                     "with --width and --height arguments!")
 
     original_image = open_image(args.orig_image_path)
     if original_image is not None:
@@ -62,6 +65,6 @@ if __name__ == '__main__':
             new_image_path = generate_path_to_image(args.orig_image_path, resized_image)
         else:
             new_image_path = args.output
-        save_image(resized_image, new_image_path)
+        resized_image.save(new_image_path)
     else:
         print('File {} not found'.format(args.orig_image_path))
